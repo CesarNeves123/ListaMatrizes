fc1d5a38b70094f30d387059899cc8163f110edd
CesarNeves123 <Cesartoledonves26@gmail.com>
2024-10-31 09:03:15 -0300

    Lista_matriezes

---
+++ Lista_Matrizes/Exercicio1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BibliotecaMatrizes;
+class Exercicio1
+{
+    public static void Main()
+    {
+        int linhas, colunas;
+
+        Console.WriteLine("Matrizes");
+        Console.WriteLine("Quantidade de linhas e colunas");
+        linhas = int.Parse(Console.ReadLine());
+        colunas = int.Parse(Console.ReadLine());
+        int[,] matriz = new int[linhas, colunas]; // criando a matriz
+        Console.WriteLine("Entre com dados para a matriz");
+        Listamatriz.gera(matriz);
+        Console.WriteLine("Dados da matriz");
+        Listamatriz.mostra(matriz);
+
+        Console.ReadKey();
+
+    }
+}
